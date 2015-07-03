516b9ea173bb354eaec27dcae94ff5a62a7bb1c5
yijiantingyu <729754029@qq.com>
2015-07-04 00:13:54 +0800

    1

--- HelloIOSGame/GameScene.swift
+++ HelloIOSGame/GameScene.swift
@@ -11,31 +11,31 @@ import SpriteKit
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+        // let myLabel = SKLabelNode(fontNamed:"Chalkduster")
+        // myLabel.text = "Hello, World!";
+         //  myLabel.fontSize = 30;
+      //   myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
         
-        self.addChild(myLabel)
+       // self.addChild(myLabel)
     }
     
     override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
         /* Called when a touch begins */
         
-        for touch in (touches as! Set<UITouch>) {
-            let location = touch.locationInNode(self)
+      //  for touch in (touches as! Set<UITouch>) {
+           //  let location = touch.locationInNode(self)
             
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
+            //let sprite = SKSpriteNode(imageNamed:"Spaceship")
             
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
+          //  sprite.xScale = 0.5
+         //   sprite.yScale = 0.5
+         //   sprite.position = location
             
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
+          //  let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
             
-            sprite.runAction(SKAction.repeatActionForever(action))
+          //  sprite.runAction(SKAction.repeatActionForever(action))
             
-            self.addChild(sprite)
+          //  self.addChild(sprite)
         }
     }
    
